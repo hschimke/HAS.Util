7cc8d343a1283c089c3035e01dfab9a17502f7b3
Henry A Schimke <henry.schimke+github@gmail.com>
2010-12-18 08:53:27 -0600

    initial commit with code

---
+++ Extensions.cs
@@ -0,0 +1,94 @@
+﻿// Copyright 2010 Henry A Schimke.
+// See LICENSE for details.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HAS.Util
+{
+    public static class Extensions
+    {
+        #region Required Local Functions
+
+        private const string template_leadin = "{{";
+        private const string template_leadout = "}}";
+
+        private static StringBuilder SetApplyTemplate(StringBuilder target, string template, string value, string prefix)
+        {
+            target.Replace(prefix + template_leadin + template + template_leadout, value);
+            return target;
+        }
+
+        #endregion
+
+        #region Extension Methods
+
+        public static string ApplyTemplate(this String str, string template_name, string template_value, string template_prefix)
+        {
+            StringBuilder sb = new StringBuilder(str);
+            SetApplyTemplate(sb, template_name, template_value, template_prefix);
+            return sb.ToString();
+        }
+
+        public static string ApplyTemplate(this String str, Dictionary<string, object> template_applicables, string template_prefix)
+        {
+            StringBuilder sb = new StringBuilder(str);
+
+            foreach (var item in template_applicables)
+            {
+                SetApplyTemplate(sb, item.Key, System.Convert.ToString(item.Value), template_prefix);
+            }
+
+            return sb.ToString();
+        }
+
+        public static string ApplyTemplate(this String str, object template_applicables, string template_prefix)
+        {
+            System.Reflection.PropertyInfo[] prop_list = template_applicables.GetType().GetProperties();
+
+            StringBuilder sb = new StringBuilder(str);
+
+            foreach (var prop in prop_list)
+            {
+                SetApplyTemplate(sb, prop.Name, System.Convert.ToString(prop.GetValue(template_applicables, null)), template_prefix);
+            }
+
+            return sb.ToString();
+        }
+
+        public static string ApplyTemplate(this String str, string template_name, string template_value)
+        {
+            return ApplyTemplate(str, template_name, template_value, String.Empty);
+        }
+
+        public static string ApplyTemplate(this String str, Dictionary<string, object> template_applicables)
+        {
+            return ApplyTemplate(str, template_applicables, String.Empty);
+        }
+
+        public static string ApplyTemplate(this String str, object template_applicables)
+        {
+            return ApplyTemplate(str, template_applicables, String.Empty);
+        }
+
+        public static string StripHtml(this String str, bool aggressive)
+        {
+            string pattern = aggressive ? @"<(.|\n)*?>" : @"</?(?i:script|embed|object|frameset|frame|iframe|meta|link|style|param|comment|listing|noscript|plaintext|xmp)(.|\n)*?>";
+
+            return Regex.Replace(str, pattern, String.Empty);
+        }
+
+        #endregion
+
+        #region Required External Functions
+
+        public static Dictionary<string, object> CreateTemplateSet()
+        {
+            return new Dictionary<string, object>();
+        }
+
+        #endregion
+    }
+}
---
+++ TagUri.cs
@@ -0,0 +1,50 @@
+﻿// Copyright 2010 Henry A Schimke.
+// See LICENSE for details.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HAS.Util
+{
+    public class TagUri
+    {
+        protected const string TAG_SCHEME = "tag";
+
+        public static Uri Create(string authority_part, string authority_date_part, params string[] specifics)
+        {
+            string format = "{{scheme}}:{{authority}},{{authority_date}}{{combined_specifics}}";
+            string specific_format = ":{{part}}";
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string p in specifics)
+            {
+                sb.Append(specific_format.ApplyTemplate("part", p));
+            }
+
+            Uri new_uri = new Uri(
+                format.ApplyTemplate(new
+                {
+                    scheme = TAG_SCHEME,
+                    authority = authority_part,
+                    authority_date = authority_date_part,
+                    combined_specifics = sb.ToString()
+                })
+            );
+
+            return new_uri;
+        }
+
+        public static Uri CreateWithFragment(string authority_part, string authority_date_part, string fragment, params string[] specifics)
+        {
+            string base_uri = Create(authority_part, authority_date_part, specifics).ToString();
+
+            string fragment_format = "#{{part}}";
+
+            string final_uri_string = base_uri + fragment_format.ApplyTemplate("part", fragment);
+
+            return new Uri(final_uri_string);
+        }
+    }
+}
